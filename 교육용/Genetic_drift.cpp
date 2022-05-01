0d5deb6a23cac58d8bb0cededac14b0f10cc6de6
성진 임 <dltjwl123@korea.ac.kr>
2022-05-02 00:05:41 +0900

    초기 커밋.

---
+++ 교육용/Genetic_drift.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <random>
+using namespace std;
+string arr[3000][6];
+int arr_row = 3000, arr_col = 6;
+void input(fstream& fs) {
+	string str;
+	int i = 0, j = 0;
+	getline(fs, str, ',');
+	while (!fs.eof()) {
+		getline(fs, str, ',');
+		if (str.size() > 10) {
+			while (str.back() != '\n') str.pop_back();
+			str.pop_back();
+		}
+		if (str.size() < 10) {
+			int zero = 10 - str.size();
+			string tmp;
+			while (zero--) tmp.push_back('0');
+			str = tmp + str;
+		}
+		arr[i][j++] = str;
+		if (j == 6) {
+			i++;
+			j = 0;
+		}
+	}
+	fs.close();
+}
+int get_p(string& str, const int p) {
+	int count = 0;
+	for (int i = 0; i < 10; i++) {
+		if (str[i] - '0' < p) count++;
+	}
+	return count;
+}
+pair<int,int> find_initial_state() {
+	vector<pair<int,int>> initial;
+	for (int i = 0; i < arr_row; i++) {
+		for (int j = 0; j < arr_col; j++) {
+			string tmp = arr[i][j];
+			if (get_p(tmp, 5) == 5) {
+				initial.push_back({ i,j });
+			}
+		}
+	}
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> dis(0, initial.size() - 1);
+	return initial[dis(gen)];
+}
+void genetic_drift(const int initial_p, const pair<int,int> index) {
+	int p = initial_p, total = 0;
+	int row = index.first, col = index.second;
+	string cur = arr[row][col];
+	char col_alpha;
+	if (row < 1000) col_alpha = 'A';
+	else if (row >= 1000 && row < 2000) col_alpha = 'G';
+	else col_alpha = 'M';
+	col_alpha = col_alpha + col;
+	cout << "seed = " << (row + 1) % 1000 << col_alpha << "\n";
+	while (p > 0 && p < 10) {
+		total++;
+		cout << "t = " << total << "\n";
+		p = get_p(cur, p);
+		if (p < 10) cout << "(" << cur << ", 0." << p << ")" << "\n";
+		else  cout << "(" << cur << ", 1)" << "\n";
+		col++;
+		if (col == 18) {
+			row++;
+			if (row == 1000) row = 0;
+			col = 0;
+		}
+		cur = arr[row][col];
+	}
+	cout << "ÃÑ ¼¼´ë ¼ö: " << total << "\n";
+}
+void print() {
+	for (int i = 0; i < arr_row; i++) {
+		for (int j = 0; j < arr_col; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+int main() {
+	fstream fs;
+	fs.open("test.csv", ios::in);
+	input(fs);
+	pair<int,int> initial_index = find_initial_state();
+	genetic_drift(5, initial_index);
+}

6be87264b9f5ae87c43a3665df912e37bb1d5056
성진 임 <dltjwl123@korea.ac.kr>
2022-05-02 01:17:53 +0900

    반복기능추가

--- 교육용/Genetic_drift.cpp
+++ 교육용/Genetic_drift.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <random>
+#include <stdlib.h>
 using namespace std;
 string arr[3000][6];
 int arr_row = 3000, arr_col = 6;
@@ -37,8 +38,7 @@ int get_p(string& str, const int p) {
 	}
 	return count;
 }
-pair<int,int> find_initial_state() {
-	vector<pair<int,int>> initial;
+void initial_state(vector<pair<int, int>>& initial) {
 	for (int i = 0; i < arr_row; i++) {
 		for (int j = 0; j < arr_col; j++) {
 			string tmp = arr[i][j];
@@ -47,6 +47,8 @@ pair<int,int> find_initial_state() {
 			}
 		}
 	}
+}
+pair<int, int> random_state(vector<pair<int, int>>& initial) {
 	random_device rd;
 	mt19937 gen(rd());
 	uniform_int_distribution<int> dis(0, initial.size() - 1);
@@ -61,22 +63,23 @@ void genetic_drift(const int initial_p, const pair<int,int> index) {
 	else if (row >= 1000 && row < 2000) col_alpha = 'G';
 	else col_alpha = 'M';
 	col_alpha = col_alpha + col;
-	cout << "seed = " << (row + 1) % 1000 << col_alpha << "\n";
+	if ((row + 1) % 10 != 0) cout << (row + 1) % 1000 << col_alpha << "\n";
+	else cout << 1000 << col_alpha << "\n";
 	while (p > 0 && p < 10) {
-		total++;
 		cout << "t = " << total << "\n";
+		total++;
 		p = get_p(cur, p);
 		if (p < 10) cout << "(" << cur << ", 0." << p << ")" << "\n";
 		else  cout << "(" << cur << ", 1)" << "\n";
 		col++;
-		if (col == 18) {
+		if (col == arr_col) {
 			row++;
-			if (row == 1000) row = 0;
+			if (row % 1000 == 0) row -= 1000;
 			col = 0;
 		}
 		cur = arr[row][col];
 	}
-	cout << "총 세대 수: " << total << "\n";
+	cout << "총 세대 수: " << total - 1 << "\n";
 }
 void print() {
 	for (int i = 0; i < arr_row; i++) {
@@ -90,6 +93,14 @@ int main() {
 	fstream fs;
 	fs.open("test.csv", ios::in);
 	input(fs);
-	pair<int,int> initial_index = find_initial_state();
-	genetic_drift(5, initial_index);
+	vector<pair<int, int>> initial;
+	initial_state(initial);
+	bool check = true;
+	while (check) {
+		pair<int, int> initial_index = random_state(initial);
+		genetic_drift(5, initial_index);
+		cout << "1: 계속하기 0: 종료\n";
+		cin >> check;
+	}
+	return 0;
 }
